e8ac1973a7b7caa25347da9e72e306139c6c3690
赵安家 <anjia0532@gmail.com>
2020-04-30 16:55:25 +0800

    refactor(time): refactor TimeEnhancement

--- elastalert_enhancements/TimeEnhancement.py
+++ elastalert_enhancements/TimeEnhancement.py
@@ -4,9 +4,12 @@ from elastalert.enhancements import BaseEnhancement
 class TimeEnhancement(BaseEnhancement):
     def process(self, match):
         self.local_time = self.rule.get('local_time_feild', 'local_time')
+        self.local_starttime = self.rule.get('local_starttime_feild', 'local_starttime')
+        self.local_endtime = self.rule.get('local_endtime_feild', 'local_endtime')
         self.ts_field = self.rule.get('timestamp_field', '@timestamp')
-        match[self.local_time] = pretty_ts(match[self.ts_field])
-        lt = self.rule.get('use_local_time')
-        match_ts = lookup_es_key(match, self.ts_field)
-        match["local_starttime"] = pretty_ts(dt_to_ts(ts_to_dt(match_ts) - self.rule['timeframe']), lt)
-        match["local_endtime"] = pretty_ts(match_ts, lt)
+        lt = self.rule.get('use_local_time',"False")
+
+        match_ts = match[self.ts_field]
+        match[self.local_time] = pretty_ts(match_ts, lt)
+        match[self.local_starttime] = pretty_ts(dt_to_ts(ts_to_dt(match_ts) - self.rule['timeframe']), lt)
+        match[self.local_endtime] = match[self.local_time]
